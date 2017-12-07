94da6591a91618d5775db1ef6c0922452e14c38a
Shaun Wahle <shaun.wahle@gmail.com>
2017-12-07 14:20:46 -0800

    adds header and footer

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/campgrounds', function(req, res) {
         { name: 'Salmon Creek', image: 'https://cdn.pixabay.com/photo/2015/07/10/17/24/night-839807_1280.jpg' }, { name: 'Eastfork', image: 'https://cdn.pixabay.com/photo/2017/11/16/09/59/camping-2953935_1280.jpg' }, { name: 'Lake Bluthe', image: 'https://cdn.pixabay.com/photo/2017/08/04/20/04/camping-2581242_1280.jpg' }
     ]
 
-    res.render('campgrounds');
+    res.render('campgrounds', { campgrounds: campgrounds });
 })
 
 app.listen(3000, function() {
