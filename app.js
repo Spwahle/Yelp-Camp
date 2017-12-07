ff56ec82971c8a3834e3f963e9560f5c64ad114b
Shaun Wahle <shaun.wahle@gmail.com>
2017-12-07 11:38:22 -0800

    adds campgrounds

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.get('/', function(req, res) {
     res.render('landing');
 });
 
+app.get('/campgrounds', function(req, res) {
+    let campgrounds = [
+        { name: 'Salmon Creek', image: 'https://cdn.pixabay.com/photo/2015/07/10/17/24/night-839807_1280.jpg' }, { name: 'Eastfork', image: 'https://cdn.pixabay.com/photo/2017/11/16/09/59/camping-2953935_1280.jpg' }, { name: 'Lake Bluthe', image: 'https://cdn.pixabay.com/photo/2017/08/04/20/04/camping-2581242_1280.jpg' }
+    ]
+
+    res.render('campgrounds');
+})
+
 app.listen(3000, function() {
     console.log('The YelpCamp Server has started');
 });
