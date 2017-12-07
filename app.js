1d5aae3c4b301d98f9af21a36d3a2777fc198103
Shaun Wahle <shaun.wahle@gmail.com>
2017-12-07 11:27:51 -0800

    adds starting page

---
+++ app.js
@@ -0,0 +1,10 @@
+let express = require('express');
+let app = express();
+
+app.get('/', function(req, res) {
+    res.send('this will be the landing page');
+});
+
+app.listen(3000, function() {
+    console.log('The YelpCamp Server has started');
+});
